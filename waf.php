3fc0c03b333084eb40dbca8e8c5b9c4d45512482
Juneah <74465835+Juneah@users.noreply.github.com>
2021-01-02 13:04:42 +0800

    Update waf.php
    修改变量名防止跟其他页面变量冲突

--- waf.php
+++ waf.php
@@ -2,16 +2,16 @@
 error_reporting(0);
 date_default_timezone_set('PRC'); 
 #定义WEB物理路径
-define('Dir' , dirname(__DIR__));
+define('JDir' , dirname(__DIR__));
 
 #等级
-$level=3;
+$jlevel=3;
 #日志功能
 $log_status=1;
 #waf功能
 $waf_status=1;
 #定义日志路径(默认为web路径下jlog.txt)
-$log_path=Dir.'/jlog.txt';
+$log_path=JDir.'/jlog.txt';
 
 function JLog(){
 	global $log_path;
@@ -38,14 +38,14 @@ function JLog(){
 }
 
 function addslashes_deep($string){
-	global $level;
+	global $jlevel;
 	if (empty($string)){
 		return $string;
-	}elseif($level>=2){
+	}elseif($jlevel>=2){
 		#等级二替换规则可自行修改
 		$math='/select\b|insert\b|flag\b|union\b|<\?\b|\?>|update\b|drop\b|and\b|delete\b|dumpfile\b|outfile\b|load_file|rename\b|`|\.\/|floor\(|extractvalue|updatexml|name_const|multipoint\(|base64_decode|eval\(|assert\(|file_put_contents|fwrite|curl|system|passthru|exec|system|chroot|scandir|chgrp|chown|shell_exec|proc_open|proc_get_status|popen|ini_alter|ini_restorei/i';
 		return is_array($string) ? array_map('addslashes_deep', $string) : preg_replace($math,'_',$string);
-	}elseif($level>=4){
+	}elseif($jlevel>=4){
 		return is_array($string) ? array_map('addslashes_deep', $string) : preg_replace('/[^\w]/','',$string);
 	}else{
 		return is_array($string) ? array_map('addslashes_deep', $string) : htmlspecialchars(addslashes(stripslashes($string)));
@@ -53,8 +53,8 @@ function addslashes_deep($string){
 }
 
 function JWaf(){
-	global $level;
-	if($level>=1){
+	global $jlevel;
+	if($jlevel>=1){
 		if(!empty($_GET)) $_GET = addslashes_deep($_GET);
 		if(!empty($_POST)) $_POST = addslashes_deep($_POST);
 		if(!empty($_REQUEST)) $_REQUEST = addslashes_deep($_REQUEST);
@@ -62,7 +62,7 @@ function JWaf(){
 		if(!empty($_SERVER)) $_SERVER = addslashes_deep($_SERVER);
 	}
 	
-	if($level>=3){
+	if($jlevel>=3){
 		if(!empty($_FILES)) {
 			foreach($_FILES as $k=>$v){
 				#修改上传文件名防止sql注入
@@ -79,7 +79,7 @@ if($log_status) JLog();
 
 if($waf_status){
 	#等级四
-	if($level>=4){
+	if($jlevel>=4){
 		foreach(get_defined_vars() as $k=>$v)
 		{
 			$$k=addslashes_deep($$k);
